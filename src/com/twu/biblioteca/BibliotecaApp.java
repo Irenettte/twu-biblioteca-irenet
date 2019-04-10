cc60fe35fb24292607f7840456c52471f2490d52
Irene Torres <irene@Irenes-MacBook-Pro.local>
2019-04-10 09:38:40 +0200

    Simplified the requirements from stories 1.1 and 1.2 doing them from the Biblioteca App main | refactoring from there

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,10 +1,18 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         String welcomeMessage = "Welcome to Biblioteca. You are one-stop-shop away from great book titles in Bangalore!";
+        List<String> books = new ArrayList();
+        books.add("The Lord of the Rings");
+        books.add("The Metamorphosis");
 
         System.out.println(welcomeMessage);
+        System.out.println(books);
+
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -20,4 +20,28 @@ public class BibliotecaAppTest {
 
         assertEquals(expectedString, outContent.toString());
     }
+    @Test
+    public void shouldDisplayWelcomeMessageAndBook() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String expectedString = "Welcome to Biblioteca. You are one-stop-shop away from great book titles in Bangalore!\nThe Lord of the Rings\n";
+
+        BibliotecaApp app = new BibliotecaApp();
+        app.main(null);
+
+        assertEquals(expectedString, outContent.toString());
+    }
+    @Test
+    public void shouldDisplayWelcomeMessageAndListBooks() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String expectedString = "Welcome to Biblioteca. You are one-stop-shop away from great book titles in Bangalore!\n[The Lord of the Rings, The Metamorphosis]\n";
+
+        BibliotecaApp app = new BibliotecaApp();
+        app.main(null);
+
+        assertEquals(expectedString, outContent.toString());
+    }
+
+
 }
