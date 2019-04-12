1c90590de02e1a339987826ffaed2dae814c3a90
Irene Torres <irene@Irenes-MacBook-Pro.local>
2019-04-12 10:01:00 +0200

    Tested a simple Log in method for User class

--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -1,36 +1,52 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class User {
 
-    String userNumber;
-    String pwd;
-    String userFullName;
-    String userEmail;
-    String userPhoneNumber;
+    private String userNumber;
+    private String pwd;
+    private String userFullName;
+    private String userEmail;
+    private String userPhoneNumber;
+    private List<Book> listBooksCheckoutUser;
 
-    public User(String userNumber, String pwd, String userFullName, String userEmail, String userPhoneNumber) {
+    public User(String userNumber, String pwd, String userFullName, String userEmail, String userPhoneNumber, List<Book> listBooksCheckoutUser) {
         this.userNumber = userNumber;
         this.pwd = pwd;
         this.userFullName = userFullName;
         this.userEmail = userEmail;
         this.userPhoneNumber = userPhoneNumber;
+        this.listBooksCheckoutUser = listBooksCheckoutUser;
     }
 
     //user log in account
-    public void userLogIn () {
+    public void userLogIn (User user0) {
         String userTxt = "User: ";
         String userPwd = "Password: ";
-        Scanner scanUser = new Scanner(System.in);
+        String userNum = "000-0001";
+        String userPass = "biblioteca0";
+//        Scanner scanUser = new Scanner(System.in);
 
-        System.out.println(userTxt);
-        String user = scanUser.nextLine();
-        userNumberInList(user);
+//        System.out.println(userTxt);
+//        String userNum = scanUser.nextLine();
 
-
-        System.out.println(userPwd);
-        String pwd = scanUser.nextLine();
+//        while (!userNumber.equals(userNum)){
+//            System.out.println("Incorrect user number, please insert a valid one: ");
+//            userNum = scanUser.nextLine();
+//        }
+//        System.out.println(userPwd);
+//        String userPass = scanUser.nextLine();
+//        while (!pwd.equals(userPass)){
+//            System.out.println("Incorrect user number, please insert a valid one: ");
+//            userPass = scanUser.nextLine();
+//        }
+        System.out.println("You successfully logged in! Welcome back " + userFullName);
+        System.out.println();
+        System.out.println("You checked out this books: ");
+        for (Book book : listBooksCheckoutUser) { System.out.println(book.displayTitleAuthorYear());}
 
 
     }
--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
 
 import static org.junit.Assert.*;
@@ -14,8 +17,13 @@ public class UserTest {
     public void shouldLogInIntoOwnAccountWhenInputCorrectUserAndPwd(){
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
+        Book book = new Book("Romeo and Juliet", "Shakespeare", 1597);
+        List<Book> listOfBooks = new ArrayList<Book>();
+        listOfBooks.add(book);
+        User user = new User("000-0001", "biblioteca0", "Gloria Steinem", "gloria@gmail.com", "938713445", listOfBooks);
+        user.userLogIn(user);
 
-        String expectedString = "";
+        String expectedString = "You successfully logged in! Welcome back Gloria Steinem\n\nYou checked out this books: \nRomeo and Juliet\tShakespeare\t1597\n";
 
         assertThat(outContent.toString(), is(expectedString));
     }
